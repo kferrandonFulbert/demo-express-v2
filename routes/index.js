f205fcdb2f260794c8aa84059156ca42d523712a
KferrandonFulbert <98470782+kferrandonFulbert@users.noreply.github.com>
2025-01-24 09:51:42 +0100

    param to view

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {h1: "Choisir votre navigateur préféré"} );
 });
 
 module.exports = router;
